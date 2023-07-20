b29e3c460394650037d569dad446d3e6f1c16ce9
xuzhilei <1159523352@qq.com>
2023-07-20 14:06:11 +0800

    add

--- EchoOwnTools.cpp
+++ EchoOwnTools.cpp
@@ -3,8 +3,78 @@
 EchoOwnTools::EchoOwnTools(QWidget *parent) : QMainWindow(parent)
 {
     ui.setupUi(this);
+
+	for (auto _button : findChildren<QPushButton*>())
+	{
+		if (nullptr != _button)
+		{
+			connect(_button, &QPushButton::clicked, this, &EchoOwnTools::onPushButtonClicked);
+		}
+	}
+	for (auto _spinbox : findChildren<QSpinBox*>())
+	{
+		if (nullptr != _spinbox)
+		{
+			connect(_spinbox, SIGNAL(valueChanged(int)), this, SLOT(onSpinboxValueChanged(int)));
+		}
+	}
+	for (auto _text : findChildren<QTextEdit*>())
+	{
+		if (nullptr != _text)
+		{
+			connect(_text, SIGNAL(textChanged()), this, SLOT(onTextEditValueChanged()));
+		}
+	}
+	for (auto _cmb : findChildren<QComboBox*>())
+	{
+		if (nullptr != _cmb)
+		{
+			connect(_cmb, SIGNAL(currentIndexChanged(int)), this, SLOT(onComboxIndexChanged(int)));
+		}
+	}
+
 }
 
 EchoOwnTools::~EchoOwnTools()
 {
+
+
+}
+
+void EchoOwnTools::onPushButtonClicked()
+{
+	QPushButton* _cur_senser = static_cast<QPushButton*>(sender());
+	if (nullptr == _cur_senser)
+	{
+		return;
+	}
+
+	if (ui.LazySingleton == _cur_senser)
+	{
+		on_LazySingleton_clicked();
+	}
+
+
+}
+
+void EchoOwnTools::onSpinboxValueChanged(int value)
+{
+
+}
+
+void EchoOwnTools::onComboxIndexChanged(int index)
+{
+
+}
+
+void EchoOwnTools::onTextEditValueChanged()
+{
+
+}
+
+
+
+void EchoOwnTools::on_LazySingleton_clicked()
+{
+
 }
--- EchoOwnTools.h
+++ EchoOwnTools.h
@@ -1,6 +1,9 @@
 ﻿#pragma once
 
 #include <QtWidgets/QMainWindow>
+#include <QSpinBox>
+#include <QTextEdit>
+#include <QComboBox>
 #include "ui_EchoOwnTools.h"
 
 //#include "lazysingleton.h"
@@ -9,13 +12,18 @@ class EchoOwnTools : public QMainWindow
 {
     Q_OBJECT
 public:
-    EchoOwnTools(QWidget *parent = Q_NULLPTR);
+	Q_INVOKABLE EchoOwnTools(QWidget *parent = Q_NULLPTR);
 	~EchoOwnTools();
 
 public slots:
+	void onPushButtonClicked();
+	void onSpinboxValueChanged(int value);
+	void onComboxIndexChanged(int index);
+	void onTextEditValueChanged();
+
 	//void on_EagerSingleton_clicked();
 
-	//void on_LazySingleton_clicked();
+	void on_LazySingleton_clicked();
 
 	// void on_FactoryMethod_clicked();
 
